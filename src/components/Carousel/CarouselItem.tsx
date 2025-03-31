7c4a0c660483657354cda7fd97189e4f6fabdb07
Le Dinh Tu <ledinhtu880@gmail.com>
2025-03-31 20:38:04 +0700

    feat: add tippy/reactjs to display tooltip

--- src/components/Carousel/CarouselItem.tsx
+++ src/components/Carousel/CarouselItem.tsx
@@ -1,17 +1,36 @@
 import clsx from "clsx";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import { Link } from "react-router-dom";
+
 import styles from "./Carousel.module.scss";
 import { MovieProps } from "@/types/index";
 import Image from "@/components/Image";
 
 const CarouselItem = ({ item }: { item: MovieProps }) => {
 	return (
-		<div key={item.id} className={clsx(styles["carousel-item"])}>
-			<Image
-				src={item.poster_url}
-				alt={item.title}
-				className={clsx(styles["carousel-image"])}
-			/>
-			<h3 className={clsx(styles["carousel-title"])}>{item.title}</h3>
+		<div className={clsx(styles["carousel-item-wrapper"])}>
+			<div key={item.id} className={clsx(styles["carousel-item"])}>
+				<Image
+					src={item.poster_url}
+					alt={item.title}
+					className={clsx(styles["carousel-image"])}
+				/>
+				<div className={clsx(styles["carousel-badge"])}>
+					<span>{item.age_rating}</span>
+				</div>
+			</div>
+			<h3>
+				<Tippy content={item.title} placement="bottom">
+					<Link
+						to="#"
+						className={clsx(styles["carousel-title"])}
+						title={item.title}
+					>
+						{item.title}
+					</Link>
+				</Tippy>
+			</h3>
 		</div>
 	);
 };
